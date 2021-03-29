52349f594ebc78c43659c0a684a92908aee98a95
TheRealColinInns <colin.inns@me.com>
2021-03-29 09:55:17 -0700

    fixed count bug

--- src/main/java/SearchQuery.java
+++ src/main/java/SearchQuery.java
@@ -24,19 +24,19 @@ public class SearchQuery {
 					if(wordKey.equals(queryWord)) {
 						int resultCount = 0;
 						if(pathCount.containsKey(pathKey)) {
-							//System.out.println("Adding1: "+mainMap.get(wordKey).get(pathKey).size()+" to path: "+pathKey);
-							//resultCount = mainMap.get(wordKey).get(pathKey).size();
-							//pathCount.put(pathKey, resultCount);
-						}
-						
-						else {
-							
 							if(pathCount.get(pathKey)!=null) {
 								resultCount = pathCount.get(pathKey);
 							}
+							else {
+								resultCount = 0;
+							}
 							resultCount+=mainMap.get(wordKey).get(pathKey).size();
-							//System.out.println("Adding2: "+resultCount+" to path: "+pathKey);
-							pathCount.put(pathKey, resultCount);
+							//System.out.println("Putting 1: "+resultCount+" to path: "+pathKey);
+							pathCount.replace(pathKey, resultCount);
+						}
+						else {
+							//System.out.println("Putting 2: "+mainMap.get(wordKey).get(pathKey).size()+" to path: "+pathKey);
+							pathCount.put(pathKey, mainMap.get(wordKey).get(pathKey).size());
 						}
 						
 					}
@@ -47,13 +47,13 @@ public class SearchQuery {
 			}
 		}
 		//System.out.println(pathCount.toString());
-		for(String pathKey:pathCount.keySet()) {
-			if(pathCount.get(pathKey)>0) {
-				wordCount = wordCountMap.get(pathKey);
+		for(String countKey:pathCount.keySet()) {
+			if(pathCount.get(countKey)>0) {
+				wordCount = wordCountMap.get(countKey);
 				ArrayList<String> singleQueryResult = new ArrayList<String>();
-				singleQueryResult.add(String.valueOf(pathCount.get(pathKey)/wordCount));
-				singleQueryResult.add(String.valueOf(wordCount));
-				singleQueryResult.add(pathKey);
+				singleQueryResult.add(String.valueOf(pathCount.get(countKey)/wordCount));
+				singleQueryResult.add(String.valueOf(pathCount.get(countKey)));
+				singleQueryResult.add(countKey);
 				queryResult.add(singleQueryResult);
 			}
 		}
@@ -64,43 +64,76 @@ public class SearchQuery {
 			}
 		}
 		if(queryResult.isEmpty()) {
-			return null;
+			return queryResult;
 		}
 		//System.out.println("After: "+queryResult.toString());
 		return resultSorter(queryResult);
 	}
 	public static ArrayList<ArrayList<String>> partialSearch(Map<String, Map<String, Collection<Integer>>> mainMap, Map<String, Integer> wordCountMap, TreeSet<String> querySet){
 		double wordCount;
+		//System.out.println("Query Set: "+querySet);
+		
 		ArrayList<ArrayList<String>> queryResult = new ArrayList<ArrayList<String>>();
+		Map<String, Integer> pathCount = new HashMap<String, Integer>();
+		
+		
 		for(String wordKey:mainMap.keySet()) {
-			//System.out.println("Main Key: "+mainKey);
 			for(String pathKey:mainMap.get(wordKey).keySet()) {
+				
 				//System.out.println("keySet: "+flippedMap.get(mainKey).keySet());
-				int resultCount = 0;
 				for(String queryWord:querySet) {
-					//System.out.println("Result Count Check: "+queryWord+" | "+secondaryKey);
-					if(wordKey.contains(queryWord)) {
-						resultCount+=mainMap.get(wordKey).get(pathKey).size();
+					//System.out.println("Result Count Check: "+queryWord+" | "+wordKey);
+					if(queryWord.contains(wordKey)) {
+						int resultCount = 0;
+						if(pathCount.containsKey(pathKey)) {
+							if(pathCount.get(pathKey)!=null) {
+								resultCount = pathCount.get(pathKey);
+							}
+							else {
+								resultCount = 0;
+							}
+							resultCount+=mainMap.get(wordKey).get(pathKey).size();
+							//System.out.println("Putting 1: "+resultCount+" to path: "+pathKey);
+							pathCount.replace(pathKey, resultCount);
+						}
+						else {
+							//System.out.println("Putting 2: "+mainMap.get(wordKey).get(pathKey).size()+" to path: "+pathKey);
+							pathCount.put(pathKey, mainMap.get(wordKey).get(pathKey).size());
+						}
+						
 					}
 				
 				}
-				if(resultCount>0) {
-					wordCount = wordCountMap.get(pathKey);
-					ArrayList<String> singleQueryResult = new ArrayList<String>();
-					singleQueryResult.add(String.valueOf(resultCount/wordCount));
-					singleQueryResult.add(String.valueOf(wordCount));
-					singleQueryResult.add(pathKey);
-					queryResult.add(singleQueryResult);
-				}
+				
 			
 			}
 		}
-		
+		//System.out.println(pathCount.toString());
+		for(String countKey:pathCount.keySet()) {
+			if(pathCount.get(countKey)>0) {
+				wordCount = wordCountMap.get(countKey);
+				ArrayList<String> singleQueryResult = new ArrayList<String>();
+				singleQueryResult.add(String.valueOf(pathCount.get(countKey)/wordCount));
+				singleQueryResult.add(String.valueOf(pathCount.get(countKey)));
+				singleQueryResult.add(countKey);
+				queryResult.add(singleQueryResult);
+			}
+		}
+		//System.out.println("Before: "+queryResult.toString());
+		for(int i = 0; i<queryResult.size(); i++) {
+			if(queryResult.get(i).isEmpty()) {
+				queryResult.remove(i);
+			}
+		}
+		if(queryResult.isEmpty()) {
+			return queryResult;
+		}
+		//System.out.println("After: "+queryResult.toString());
 		return resultSorter(queryResult);
 	}
 	private static ArrayList<ArrayList<String>> resultSorter(ArrayList<ArrayList<String>> helperList){
 		ArrayList<ArrayList<String>> sortedList = new ArrayList<ArrayList<String>>();
-		System.out.println("Original List: "+helperList.toString());
+		//System.out.println("Original List: "+helperList.toString());
 		sortedList.add(helperList.get(0));
 		//System.out.println("Helper size: "+helperList.size());
 		int maxCount = helperList.size();
@@ -124,7 +157,7 @@ public class SearchQuery {
 						//System.out.println("New Sorted List 2: "+sortedList.toString());
 					}
 					else if(Double.valueOf(helperList.get(i).get(1))==Double.valueOf(sortedList.get(j).get(1))) {
-						if(helperList.get(i).get(2).compareTo(sortedList.get(j).get(2))>0) {
+						if(myCompareTo(helperList.get(i).get(2), sortedList.get(j).get(2))>0) {
 							sortedList.add(j, helperList.get(i));
 							maxCount--;
 							if (maxCount<=1) {
@@ -132,13 +165,13 @@ public class SearchQuery {
 							}
 							//System.out.println("New Sorted List 3: "+sortedList.toString());
 						}
-						else if(helperList.get(i).get(2).compareTo(sortedList.get(j).get(2))==0) {
+						else if(myCompareTo(helperList.get(i).get(2), sortedList.get(j).get(2))==0) {
 							System.out.println("DUPLICATE ERROR| helper: "+helperList.get(i)+"    sorted: "+sortedList.get(j));
 						}
 						else  {
 							sortedList.add(helperList.get(i));
 							maxCount--;
-							if (maxCount<=1) {
+							if (maxCount<=2) {
 								break;
 							}
 							//System.out.println("New Sorted List 4: "+sortedList.toString());
@@ -148,7 +181,7 @@ public class SearchQuery {
 					else {
 						sortedList.add(helperList.get(i));
 						maxCount--;
-						if (maxCount<=1) {
+						if (maxCount<=2) {
 							break;
 						}
 						//System.out.println("New Sorted List 5: "+sortedList.toString());
@@ -158,7 +191,7 @@ public class SearchQuery {
 				else {
 					sortedList.add(helperList.get(i));
 					maxCount--;
-					if (maxCount<=1) {
+					if (maxCount<=2) {
 						break;
 					}
 					//System.out.println("New Sorted List 6: "+sortedList.toString());
@@ -168,6 +201,15 @@ public class SearchQuery {
 		}
 		//System.out.println("Final List: "+sortedList);
 		return sortedList;
+	}
+	private static int myCompareTo(String firstString, String secondString) {
+		firstString = TextParser.clean(firstString);
+		secondString = TextParser.clean(secondString);
+		return firstString.compareToIgnoreCase(secondString);
+	}
+	private static boolean partialSearcher(String wordKey, String query) {
+		
+		return false;
 	}
 }
 
